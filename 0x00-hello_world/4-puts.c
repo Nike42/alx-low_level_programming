5c5a22e9dc77487477b59a31b1d9483eb07982c6
nike42 <marynyke12@gmail.com>
2022-09-08 14:20:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Printing
  *
- * Return:0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
